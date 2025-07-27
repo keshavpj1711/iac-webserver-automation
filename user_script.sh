997170d12204cf925f7f97ad628547e35f98ff8e
keshavpj1711 <keshavpj1711@gmail.com>
2025-07-28 02:02:48 +0530

    update: fixed the startup script to ensure SSH is running

--- user_script.sh
+++ user_script.sh
@@ -3,6 +3,11 @@ yum update -y
 yum install -y httpd # Apache web server package
 systemctl start httpd
 systemctl enable httpd
+# Ensure SSH is running
+systemctl start sshd
+systemctl enable sshd
 echo "<h1>Hello from IaC!</h1>" > /var/www/html/index.html
 echo "<p>This web server was created using Pulumi and Python</p>" >> /var/www/html/index.html
 echo "<p>Instance ID: $(curl -s http://169.254.169.254/latest/meta-data/instance-id)</p>" >> /var/www/html/index.html
+echo "<p>Visit this project's codebase at: <a href='https://github.com/keshavpj1711/iac-webserver-automation'>github link</a></p>" >> /var/www/html/index.html
+echo "<p>Hope you are doing good.</p><p>-Keshav PJ</p>" >> /var/www/html/index.html
